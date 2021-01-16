804cec8d8dd82e05623d14e1acb3531976aff789
Christine Wilks <crissxross@gmail.com>
2021-01-16 09:58:11 +0000

    some strict fixes, removes .mat-typography from body so now only on mat components

--- src/app/components/youtube-video/youtube-video.component.ts
+++ src/app/components/youtube-video/youtube-video.component.ts
@@ -7,8 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class YoutubeVideoComponent implements OnInit {
 
-  @Input() videoId = '';
   apiLoaded = false;
+  @Input() videoId: string | undefined = '';
 
   constructor() { }
 
