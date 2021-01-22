574a0aafffaec9dc54f0d5b10be6d14b8d387b8e
Christine Wilks <crissxross@gmail.com>
2021-01-22 17:33:41 +0000

    adds images & some text for cx-trail

--- src/app/shared/artworks-data.ts
+++ src/app/shared/artworks-data.ts
@@ -411,7 +411,140 @@ export const artworksData: ArtWork[] = [
 
       `The R3M1XW0RX site hosts more than 500 remixes of digital art, animation, sound, music, poetry and writing.`
     ],
-    youtubeVideos: [],
+    youtubeVideos: [
+      {
+        id: 'fXI-pr55MuE',
+        name: 'Ada Lovelace Day'
+      },
+      {
+        id: 'ZxtR9IHFPDc',
+        name: 'thing'
+      },
+      {
+        id: 'LhWXXFpIyQo',
+        name: 'address unknown'
+      },
+      {
+        id: 'vuLca-eSbFw',
+        name: 'Anniversary Mix'
+      },
+      {
+        id: 'BYoYdWkn5xo',
+        name: 'Archipelago'
+      },
+      {
+        id: 'Kc9rfMApucY',
+        name: 'Barnoracle'
+      },
+      {
+        id: 'gH-LKnScz94',
+        name: 'biogenoscopic translocation'
+      },
+      {
+        id: 'i8k6FxYQm2U',
+        name: 'Botty Training'
+      },
+      {
+        id: 'VUhoJ6k0Wkw',
+        name: 'Bread Circus'
+      },
+      {
+        id: 'Q21J7W1WEDU',
+        name: 'Chance Joiner'
+      },
+      {
+        id: 'TpZXqOpE8bY',
+        name: 'easyFACTS'
+      },
+      {
+        id: '1_CQ6n8Jw8I',
+        name: 'Headgap'
+      },
+      {
+        id: 'jA8_cG0zShU',
+        name: 'ID Card'
+      },
+      {
+        id: 'BlGsa58vjW4',
+        name: 'If you go down to the woods'
+      },
+      {
+        id: 'ZofMwAySuGo',
+        name: 'Janus 2010'
+      },
+      {
+        id: 'xoOJQQbiAVw',
+        name: 'Janus'
+      },
+      {
+        id: 'uf0oCaqz9MM',
+        name: 'Letting Slip'
+      },
+      {
+        id: 'S5yCsHbo2KE',
+        name: 'Bookish Stones'
+      },
+      {
+        id: 'hX05heYHuVE',
+        name: 'exclamation mark'
+      },
+      {
+        id: 'jb_mcL1ziTw',
+        name: 'Loyalty Card'
+      },
+      {
+        id: 'W0jYBuNBvO4',
+        name: 'Mondrian Money Moon'
+      },
+      {
+        id: 'xoJ-pPF-1u4',
+        name: 'Outpost msg'
+      },
+      {
+        id: 'Lg2sf39Bw7A',
+        name: 'Point Taken'
+      },
+      {
+        id: 'dWuN2ya7QHA',
+        name: 'raw Lorem Ipsum'
+      },
+      {
+        id: 'EzzgG_hchAQ',
+        name: 'Sacrificial Penpusher'
+      },
+      {
+        id: 'rJtBxf8R1kc',
+        name: 'Shadow Capital'
+      },
+      {
+        id: 'Qnf6eBy0XGM',
+        name: 'SonoBuoy'
+      },
+      {
+        id: 'VgTyM7x1GL4',
+        name: 'Spritely Does It'
+      },
+      {
+        id: 'iFzM6HhAMQ4',
+        name: 'StoneFaceSea'
+      },
+      {
+        id: '5u6GcHuZi6U',
+        name: 'Turning Away Toward'
+      },
+      {
+        id: 'MbXhTENcjmI',
+        name: 'Used to be big'
+      },
+      {
+        id: '1Kw3fkq2ZHM',
+        name: 'WaterDragon'
+      },
+      {
+        id: '4ZjtLtd7mHE',
+        name: 'End of the Rainbow'
+      },
+    ],
     images: [''],
     pubdate: '2012',
     tech: 'HTML & video (originally included Flash)',
